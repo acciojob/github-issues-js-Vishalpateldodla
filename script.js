a05d2f85d3bf44407dbd637f7ab6d68c279e0428
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-02-15 15:28:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your code here
+const issueList = document.getElementById('issue-list');
+const pageNumber = document.getElementById('page-number');
+let currentPage = 1;
+
+// function to fetch and display issues
+async function displayIssues(page) {
+  const response = await fetch(`https://api.github.com/repositories/1296269/issues?page=${page}&per_page=5`);
+  const issues = await response.json();
+
+  issueList.innerHTML = '';
+  issues.forEach(issue => {
+    const issueName = document.createElement('li');
+    issueName.textContent = issue.title;
+    issueList.appendChild(issueName);
+  });
+}
+
+// initial display of issues on page load
+displayIssues(currentPage);
+
+// function to handle loading next page of issues
+document.getElementById('load-next').addEventListener('click', () => {
+  currentPage++;
+  pageNumber.textContent = `Page number ${currentPage}`;
+  displayIssues(currentPage);
+});
+
+// function to handle loading previous page of issues
+document.getElementById('load-prev').addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    pageNumber.textContent = `Page number ${currentPage}`;
+    displayIssues(currentPage);
+  }
+});
